1fc2ee8b6e86efec349247f154afaeef7387237c
Friedrich von Never <friedrich@fornever.me>
2025-07-05 22:28:19 +0200

    Build: compilation fixes, drop unused parts of the project template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,10 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 plugins {
-    // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
-    id("buildsrc.convention.kotlin-jvm")
-
     // Apply the Application plugin to add support for building an executable JVM application.
     application
 }
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -3,15 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 plugins {
-    // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
-    id("buildsrc.convention.kotlin-jvm")
-    // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
-    alias(libs.plugins.kotlinPluginSerialization)
+    alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
-    // Apply the kotlinx bundle of dependencies from the version catalog (`gradle/libs.versions.toml`).
-    implementation(libs.bundles.kotlinxEcosystem)
     testImplementation(kotlin("test"))
 }
